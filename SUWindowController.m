2e3adbe31dd754dab208f73e227d819e87388b6a
Stefan Neumärker <stefan@boinx.com>
2012-12-21 17:48:01 +0100

    changed window nib loading - hope this fixes the crash Refs #123

--- SUWindowController.m
+++ SUWindowController.m
@@ -13,15 +13,24 @@
 
 - (id)initWithHost:(SUHost *)host windowNibName:(NSString *)nibName
 {
-	NSString *path = [[NSBundle bundleForClass:[self class]] pathForResource:nibName ofType:@"nib"];
-	if (path == nil) // Slight hack to resolve issues with running Sparkle in debug configurations.
-	{
-		NSString *frameworkPath = [[[host bundle] sharedFrameworksPath] stringByAppendingPathComponent:@"Sparkle.framework"];
-		NSBundle *framework = [NSBundle bundleWithPath:frameworkPath];
-		path = [framework pathForResource:nibName ofType:@"nib"];
-	}
-	self = [super initWithWindowNibPath:path owner:self];	
-	return self;
+//	NSString *path = [[NSBundle bundleForClass:[self class]] pathForResource:nibName ofType:@"nib"];
+//	if (path == nil) // Slight hack to resolve issues with running Sparkle in debug configurations.
+//	{
+//		NSString *frameworkPath = [[[host bundle] sharedFrameworksPath] stringByAppendingPathComponent:@"Sparkle.framework"];
+//		NSBundle *framework = [NSBundle bundleWithPath:frameworkPath];
+//		path = [framework pathForResource:nibName ofType:@"nib"];
+//	}
+//	self = [super initWithWindowNibPath:path owner:self];
+	
+	/* looks like -initWithWindowNibPaht:owner: is should not be used with pathes inside main bundle or framework bunlde:
+
+	 Use this method if your nib file is at a fixed location (which is not inside either the file’s owner’s class’s bundle or in the application’s main bundle).
+	 
+	 Will try to load the nib by it's name.
+	 */
+	
+	self = [super initWithWindowNibName:nibName owner:self];
+	return nil;
 }
 
 @end
